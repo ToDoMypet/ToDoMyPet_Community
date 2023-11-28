71bfbec5e7462d104b36c7917d44bb227bc90796
dawonseo <oloielo@naver.com>
2023-11-28 15:09:20 +0900

    chore: swagger 파라미터 hidden 제거

--- src/main/java/com/todomypet/communityservice/controller/BoardController.java
+++ src/main/java/com/todomypet/communityservice/controller/BoardController.java
@@ -24,7 +24,7 @@ public class BoardController {
 
     @Operation(summary = "글 작성", description = "글을 작성하여 발행합니다.")
     @PostMapping("")
-    public SuccessResDTO<WritePostResDTO> WritePost(@Parameter(hidden = true) @RequestHeader String userId,
+    public SuccessResDTO<WritePostResDTO> WritePost(@RequestHeader String userId,
                                                     @RequestBody WritePostReqDTO writePostReqDTO) {
         String responseId = boardService.post(userId, writePostReqDTO);
         WritePostResDTO writePostResDTO = WritePostResDTO.builder().id(responseId).build();
@@ -33,7 +33,7 @@ public class BoardController {
 
     @Operation(summary = "피드 조회", description = "피드를 조회합니다.")
     @GetMapping("")
-    public SuccessResDTO<BoardListResDTO> getFeed(@Parameter(hidden = true) @RequestHeader String userId,
+    public SuccessResDTO<BoardListResDTO> getFeed(@RequestHeader String userId,
                                                   @RequestParam(required = false) String nextIndex,
                                                   @RequestParam(required = false, defaultValue = "20") int pageSize) {
         BoardListResDTO response = boardService.getFeed(userId, nextIndex, pageSize);
@@ -42,14 +42,14 @@ public class BoardController {
 
     @Operation(summary = "글 삭제", description = "특정 글을 삭제합니다.")
     @DeleteMapping("/{postId}")
-    public SuccessResDTO<Void> deletePost(@Parameter(hidden = true) @RequestHeader String userId, @PathVariable String postId) {
+    public SuccessResDTO<Void> deletePost(@RequestHeader String userId, @PathVariable String postId) {
         boardService.deletePost(userId, postId);
         return new SuccessResDTO<>(null);
     }
 
     @Operation(summary = "글 수정", description = "글을 수정합니다. 글 내용과 첨부 이미지를 수정할 수 있습니다.")
     @PutMapping("/{postId}")
-    public SuccessResDTO<PostUpdateResDTO> updatePost(@Parameter(hidden = true) @RequestHeader String userId, @PathVariable String postId,
+    public SuccessResDTO<PostUpdateResDTO> updatePost(@RequestHeader String userId, @PathVariable String postId,
                                                       @RequestBody PostUpdateReqDTO postUpdateReqDTO) {
         boardService.updatePost(userId, postId, postUpdateReqDTO);
         PostUpdateResDTO response = PostUpdateResDTO.builder().id(postId).build();
@@ -58,7 +58,7 @@ public class BoardController {
 
     @Operation(summary = "내가 쓴 글 조회", description = "로그인된 사용자가 작성한 글 리스트를 조회합니다.")
     @GetMapping("/my-post-list")
-    public SuccessResDTO<BoardListResDTO> getMyPostList(@Parameter(hidden = true) @RequestHeader String userId,
+    public SuccessResDTO<BoardListResDTO> getMyPostList(@RequestHeader String userId,
                                                         @RequestParam(required = false) String nextIndex,
                                                         @RequestParam(required = false, defaultValue = "20") int pageSize) {
         BoardListResDTO response = boardService.getMyPostList(userId, nextIndex, pageSize);
