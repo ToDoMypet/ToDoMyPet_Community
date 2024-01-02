222c0a124b2c1fc5488b60d3e8b5d94e2e58f4a0
dawonseo <oloielo@naver.com>
2024-01-02 19:55:57 +0900

    fix: 타임존 변경

--- src/main/java/com/todomypet/communityservice/CommunityServiceApplication.java
+++ src/main/java/com/todomypet/communityservice/CommunityServiceApplication.java
@@ -1,12 +1,20 @@
 package com.todomypet.communityservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients
 public class CommunityServiceApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommunityServiceApplication.class, args);
