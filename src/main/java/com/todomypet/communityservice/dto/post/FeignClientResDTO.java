3924174aaecadbc7696090d360f2a99bb4b72e23
dawonseo <oloielo@naver.com>
2024-03-12 23:44:45 +0900

    chore: 어노테이션 추가

--- src/main/java/com/todomypet/communityservice/dto/post/FeignClientResDTO.java
+++ src/main/java/com/todomypet/communityservice/dto/post/FeignClientResDTO.java
@@ -3,8 +3,10 @@ package com.todomypet.communityservice.dto.post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Getter
 public class FeignClientResDTO<T> {
--- src/main/java/com/todomypet/communityservice/dto/reply/SendReplyNotificationReqDTO.java
+++ src/main/java/com/todomypet/communityservice/dto/reply/SendReplyNotificationReqDTO.java
@@ -1,9 +1,15 @@
 package com.todomypet.communityservice.dto.reply;
 
 import com.todomypet.communityservice.domain.node.NotificationType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SendReplyNotificationReqDTO {
     private String userId;
     private NotificationType type;
